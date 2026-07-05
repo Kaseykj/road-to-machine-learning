4ad3a9b90f568db034c23e5856478f486e46a052
Ali Hamza Ünal <126963132+Kaseykj@users.noreply.github.com>
2026-07-05 22:05:22 +0300

    Create hypotenuse-calculator.py

---
+++ Basic-Math-and-Exercises/hypotenuse-calculator.py
@@ -0,0 +1,8 @@
+print("===========") #tasarım
+print(" a\u00B2 = b\u00B2 + c\u00B2 ")
+print("===========")
+b = input("b = ")
+c = input("c = ")
+akare = int(b)**2 + int(c)**2 
+result = akare**(1/2)
+print(f"a is = {result}")
